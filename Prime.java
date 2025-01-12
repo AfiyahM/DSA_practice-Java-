a969e79e4df14b91592fc9ea8f3a4806102939ec
AfiyahM <136073607+AfiyahM@users.noreply.github.com>
2025-01-12 22:18:34 +0530

    new

---
+++ Prime.java
@@ -0,0 +1,17 @@
+public static boolean isPrime(int n){
+    boolean isPrime=true
+    for(int i=2;i<=n;i++){
+        if(n%==0){
+
+            isPrime=false
+
+        }
+    }
+    return isPrime
+}
+
+public static void main (String[]args){
+
+    isPrime(20)
+    
+}
