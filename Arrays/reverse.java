1867a6563ee59903325b157ad61dbfd2d72bd3a7
AfiyahM <136073607+AfiyahM@users.noreply.github.com>
2025-02-24 00:10:09 +0530

    reverse

--- Arrays/reverse.java
+++ Arrays/reverse.java
@@ -1,6 +1,6 @@
  class reverse{
     public static int reverse (int [] arr){
-        int  [] temp = new int [];
+        int  [] temp = new int [arr.length];
         for( int i =0; i<arr.length;i++){
             temp[i]=arr[arr.length-i-1];
         }
