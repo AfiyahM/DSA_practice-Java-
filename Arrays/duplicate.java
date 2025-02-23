521ef6766e6e1c67c4eb4cc49f336202f6a84bd3
AfiyahM <136073607+AfiyahM@users.noreply.github.com>
2025-02-24 00:44:37 +0530

    duplicates

---
+++ Arrays/duplicate.java
@@ -0,0 +1,16 @@
+class duplicates {
+    public boolean duplicates(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+        return numSet.size() < nums.length;
+    }
+    public static void main(String [] args){
+    int [] arr ={-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(" "+ duplicates(arr));
+       
+
+
+    }
+}
