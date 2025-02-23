b1d00f9541e45d168b1d0a9739d89e8d5fbfd6ad
AfiyahM <136073607+AfiyahM@users.noreply.github.com>
2025-02-24 00:12:45 +0530

    revrse

--- Arrays/reverse.java
+++ Arrays/reverse.java
@@ -1,5 +1,5 @@
  class reverse{
-    public static int reverse (int [] arr){
+    public static  void reverse (int [] arr){
         int  [] temp = new int [arr.length];
         for( int i =0; i<arr.length;i++){
             temp[i]=arr[arr.length-i-1];
@@ -7,7 +7,7 @@
         for( int i =0; i<arr.length;i++){
             arr[i]=temp[i];
         }
-        return temp;
+    
     }
 
     
@@ -15,6 +15,9 @@
      public static void main (String []args){
          int [] arr ={1,4,5,8,21,34,15,673,89,0};
         System.out.println("The given array is"+ arr);
-        System.out.println("The reverse array is"+ reverse(arr));
+        reverse(arr); 
+        for (int i = 0; i < arr.length; i++) 
+            System.out.print(arr[i] + " ");
+    }
        
-    }}
+    }
