0e513c2dfcf6cd073ec751483ceb0c57287df1b8
AfiyahM <136073607+AfiyahM@users.noreply.github.com>
2025-02-24 00:29:54 +0530

    Max sum of subarray

--- Arrays/maxSubArray.java
+++ Arrays/maxSubArray.java
@@ -1,5 +1,5 @@
 class maxSubArray {
-    public int maxSubArray(int[] nums) {
+    public static int maxSubArray(int[] nums) {
         int ms=Integer.MIN_VALUE;
         int cs=0;
         for (int i=0; i<nums.length;i++){
@@ -14,7 +14,7 @@ class maxSubArray {
     }
     public static void main(String [] args){
     int [] arr ={-2,1,-3,4,-1,2,1,-5,4};
-        System.out.println("Max sum of subarray is"+maxSubArray(arr));
+        System.out.println("Max sum of subarray is"+ maxSubArray(arr));
        
 
 
