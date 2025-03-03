48beacabb6a7362baa872759d14c98bdc3e886a6
AfiyahM <136073607+AfiyahM@users.noreply.github.com>
2025-03-03 13:12:13 +0530

    chocolate prooblem

---
+++ Arrays/chocolate.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+ class chocolate{
+    public static int mindiff(int [] arr, int m){
+        int n=arr.length;
+        Arrays.sort(arr);
+        int mindiff=Integer.MAX_VALUE;
+        for(int i=0;i+m-1;i++){
+            int diff=arr[i+m-1]-arr[i];
+            if(diff < mindiff){
+                mindiff=diff;
+            }
+            return mindiff;
+        }
+        public static void main(String[] args) {
+        int[] arr = {7, 3, 2, 4, 9, 12, 56};
+        int m = 3;
+
+        System.out.println(findMinDiff(arr, m));
+    }
+    }
+ }
