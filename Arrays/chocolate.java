1369a3c3eaf66fd8539ec956fa1eeed9006f6192
AfiyahM <136073607+AfiyahM@users.noreply.github.com>
2025-03-03 13:15:54 +0530

    chocolate problem

--- Arrays/chocolate.java
+++ Arrays/chocolate.java
@@ -12,11 +12,11 @@ import java.util.Arrays;
             }
             return mindiff;
         }
+    }
         public static void main(String[] args) {
         int[] arr = {7, 3, 2, 4, 9, 12, 56};
         int m = 3;
 
         System.out.println(findMinDiff(arr, m));
-    }
     }
  }
