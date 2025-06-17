6d364f474f1f4664852f9cd67c1d801723c98ff0
AfiyahM <136073607+AfiyahM@users.noreply.github.com>
2025-06-17 20:58:55 +0530

    array

--- Arrays/remove_duplicate.java
+++ Arrays/remove_duplicate.java
@@ -2,14 +2,14 @@ public class RemoveDuplicates {
     public static int removeDuplicates(int[] nums) {
         if (nums.length == 0) return 0;
 
-        int i = 0; // pointer for unique elements
+        int i = 0; 
         for (int j = 1; j < nums.length; j++) {
             if (nums[j] != nums[i]) {
-                i++;               // move to next unique position
-                nums[i] = nums[j]; // update with new unique element
+                i++;              
+                nums[i] = nums[j]; 
             }
         }
-        return i + 1; // new length of array with unique elements
+        return i + 1; 
     }
 
     public static void main(String[] args) {
